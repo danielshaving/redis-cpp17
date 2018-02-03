ed0bc686a93bb9ea883c94d2069869985739bd47
danielshaving <474711079@qq.com>
2018-02-03 08:35:23 -0800

    c17

--- src/xHiredisAsync.d
+++
@@ -1,7 +0,0 @@
-xHiredisAsync.o: ../src/xHiredisAsync.cpp ../src/xHiredisAsync.h \
- ../src/xHiredis.h ../src/xSds.h ../src/xZmalloc.h ../src/all.h \
- ../src/xTcpconnection.h ../src/xBuffer.h ../src/xCallback.h \
- ../src/xChannel.h ../src/xObject.h ../src/xLog.h ../src/xUtil.h \
- ../src/xSha1.h ../src/xTcpClient.h ../src/xEventLoop.h ../src/xSocket.h \
- ../src/xEpoll.h ../src/xTimerQueue.h ../src/xTimer.h \
- ../src/xThreadPool.h
