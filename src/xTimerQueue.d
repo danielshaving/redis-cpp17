a25d333cc4fd8213c1b46a7a557eb6ca3c8c5f81
danielshaving <daniellhongkong@gmail.com>
2018-03-02 15:34:10 +0800

    Delete xTimerQueue.d

--- src/xTimerQueue.d
+++
@@ -1,4 +0,0 @@
-xTimerQueue.o: ../src/xTimerQueue.cpp ../src/xTimerQueue.h ../src/all.h \
- ../src/xChannel.h ../src/xCallback.h ../src/xTimer.h ../src/xZmalloc.h \
- ../src/xEventLoop.h ../src/xSocket.h ../src/xEpoll.h ../src/xLog.h \
- ../src/xBuffer.h
