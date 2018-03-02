ccec26808bef4fd4a8d1ee3130b8892efb91afce
danielshaving <daniellhongkong@gmail.com>
2018-03-02 15:15:46 +0800

    Delete xAcceptor.d

--- src/xAcceptor.d
+++
@@ -1,4 +0,0 @@
-xAcceptor.o: ../src/xAcceptor.cpp ../src/xAcceptor.h ../src/all.h \
- ../src/xChannel.h ../src/xEventLoop.h ../src/xSocket.h ../src/xEpoll.h \
- ../src/xLog.h ../src/xBuffer.h ../src/xTimerQueue.h ../src/xCallback.h \
- ../src/xTimer.h ../src/xZmalloc.h
