852508c9cc50c165f7a0bd6c4ba596a6286a2d0c
danielshaving <daniellhongkong@gmail.com>
2018-03-02 15:43:03 +0800

    Delete xMain.d

--- src/xMain.d
+++
@@ -1,9 +0,0 @@
-xMain.o: ../src/xMain.cpp ../src/xRedis.h ../src/all.h \
- ../src/xEventLoop.h ../src/xChannel.h ../src/xSocket.h ../src/xEpoll.h \
- ../src/xLog.h ../src/xBuffer.h ../src/xTimerQueue.h ../src/xCallback.h \
- ../src/xTimer.h ../src/xZmalloc.h ../src/xTcpConnection.h \
- ../src/xTcpServer.h ../src/xAcceptor.h ../src/xThreadPool.h \
- ../src/xSds.h ../src/xSession.h ../src/xObject.h ../src/xUtil.h \
- ../src/xSha1.h ../src/xItem.h ../src/xRdb.h ../src/xCrc64.h \
- ../src/xLzf.h ../src/xReplication.h ../src/xTcpClient.h \
- ../src/xSentinel.h ../src/xCluster.h
