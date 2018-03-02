4aa219f7b9b365e890e01473635e9c58a639cebf
danielshaving <daniellhongkong@gmail.com>
2018-03-02 15:46:27 +0800

    Delete xThread.d

--- src/xThread.d
+++
@@ -1,4 +0,0 @@
-xThread.o: ../src/xThread.cpp ../src/xThread.h ../src/all.h \
- ../src/xEventLoop.h ../src/xChannel.h ../src/xSocket.h ../src/xEpoll.h \
- ../src/xLog.h ../src/xBuffer.h ../src/xTimerQueue.h ../src/xCallback.h \
- ../src/xTimer.h ../src/xZmalloc.h
