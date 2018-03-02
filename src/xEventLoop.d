c8562a217130f9b1989aea46ae61b48457c2661c
danielshaving <daniellhongkong@gmail.com>
2018-03-02 15:33:53 +0800

    Delete xEventLoop.d

--- src/xEventLoop.d
+++
@@ -1,4 +0,0 @@
-xEventLoop.o: ../src/xEventLoop.cpp ../src/all.h ../src/xEventLoop.h \
- ../src/xChannel.h ../src/xSocket.h ../src/xEpoll.h ../src/xLog.h \
- ../src/xBuffer.h ../src/xTimerQueue.h ../src/xCallback.h ../src/xTimer.h \
- ../src/xZmalloc.h
