f26e859c6a0413a55d21935976698cace00e1f1e
danielshaving <daniellhongkong@gmail.com>
2018-03-02 15:44:49 +0800

    Delete xThreadPool.d

--- src/xThreadPool.d
+++
@@ -1,4 +0,0 @@
-xThreadPool.o: ../src/xThreadPool.cpp ../src/xThreadPool.h ../src/all.h \
- ../src/xEventLoop.h ../src/xChannel.h ../src/xSocket.h ../src/xEpoll.h \
- ../src/xLog.h ../src/xBuffer.h ../src/xTimerQueue.h ../src/xCallback.h \
- ../src/xTimer.h ../src/xZmalloc.h ../src/xThread.h
