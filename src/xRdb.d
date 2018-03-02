bbc6276464596ed9b853adc475e4d118c23f33eb
danielshaving <daniellhongkong@gmail.com>
2018-03-02 15:42:24 +0800

    Delete xRdb.d

--- src/xRdb.d
+++
@@ -1,9 +0,0 @@
-xRdb.o: ../src/xRdb.cpp ../src/xRdb.h ../src/all.h ../src/xSds.h \
- ../src/xZmalloc.h ../src/xCrc64.h ../src/xObject.h ../src/xBuffer.h \
- ../src/xLog.h ../src/xUtil.h ../src/xSha1.h ../src/xLzf.h \
- ../src/xSession.h ../src/xTcpConnection.h ../src/xCallback.h \
- ../src/xChannel.h ../src/xItem.h ../src/xRedis.h ../src/xEventLoop.h \
- ../src/xSocket.h ../src/xEpoll.h ../src/xTimerQueue.h ../src/xTimer.h \
- ../src/xTcpServer.h ../src/xAcceptor.h ../src/xThreadPool.h \
- ../src/xReplication.h ../src/xTcpClient.h ../src/xSentinel.h \
- ../src/xCluster.h
