6a80ff48477c6325f83a1ef6dbeb0a8f46b59676
danielshaving <daniellhongkong@gmail.com>
2018-03-02 15:19:10 +0800

    Delete xCluster.d

--- src/xCluster.d
+++
@@ -1,9 +0,0 @@
-xCluster.o: ../src/xCluster.cpp ../src/xCluster.h ../src/all.h \
- ../src/xObject.h ../src/xZmalloc.h ../src/xSds.h ../src/xBuffer.h \
- ../src/xLog.h ../src/xUtil.h ../src/xSha1.h ../src/xTcpClient.h \
- ../src/xEventLoop.h ../src/xChannel.h ../src/xSocket.h ../src/xEpoll.h \
- ../src/xTimerQueue.h ../src/xCallback.h ../src/xTimer.h \
- ../src/xSession.h ../src/xTcpConnection.h ../src/xItem.h ../src/xRedis.h \
- ../src/xTcpServer.h ../src/xAcceptor.h ../src/xThreadPool.h \
- ../src/xRdb.h ../src/xCrc64.h ../src/xLzf.h ../src/xReplication.h \
- ../src/xSentinel.h ../src/xCrc16.h
