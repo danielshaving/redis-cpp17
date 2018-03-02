79328a3a415cf0031a2908e7c48973504f59aa85
danielshaving <daniellhongkong@gmail.com>
2018-03-02 15:18:20 +0800

    Delete xChannel.d

--- src/xChannel.d
+++
@@ -1,4 +0,0 @@
-xChannel.o: ../src/xChannel.cpp ../src/xChannel.h ../src/all.h \
- ../src/xEventLoop.h ../src/xSocket.h ../src/xEpoll.h ../src/xLog.h \
- ../src/xBuffer.h ../src/xTimerQueue.h ../src/xCallback.h ../src/xTimer.h \
- ../src/xZmalloc.h
