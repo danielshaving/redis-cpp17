284798fc50d7cd0446183ea3f0249ce916ad1260
danielshaving <daniellhongkong@gmail.com>
2018-03-02 15:46:48 +0800

    Delete xReplication.d

--- src/xReplication.d
+++
@@ -1,9 +0,0 @@
-xReplication.o: ../src/xReplication.cpp ../src/xReplication.h \
- ../src/all.h ../src/xObject.h ../src/xZmalloc.h ../src/xSds.h \
- ../src/xBuffer.h ../src/xLog.h ../src/xUtil.h ../src/xSha1.h \
- ../src/xTcpClient.h ../src/xEventLoop.h ../src/xChannel.h \
- ../src/xSocket.h ../src/xEpoll.h ../src/xTimerQueue.h ../src/xCallback.h \
- ../src/xTimer.h ../src/xRedis.h ../src/xTcpConnection.h \
- ../src/xTcpServer.h ../src/xAcceptor.h ../src/xThreadPool.h \
- ../src/xSession.h ../src/xItem.h ../src/xRdb.h ../src/xCrc64.h \
- ../src/xLzf.h ../src/xSentinel.h ../src/xCluster.h
