ef872e26f8c8711a5cb8cd1fedf837742b028a6b
danielshaving <daniellhongkong@gmail.com>
2018-03-22 18:30:00 +0800

    Delete xHttpServer.d

--- src/xHttpServer.d
+++
@@ -1,6 +0,0 @@
-xHttpServer.o: ../src/xHttpServer.cpp ../src/xHttpServer.h ../src/all.h \
- ../src/xTcpServer.h ../src/xAcceptor.h ../src/xChannel.h \
- ../src/xEventLoop.h ../src/xSocket.h ../src/xPoll.h ../src/xLog.h \
- ../src/xBuffer.h ../src/xTimerQueue.h ../src/xCallback.h ../src/xTimer.h \
- ../src/xZmalloc.h ../src/xThreadPool.h ../src/xHttpContext.h \
- ../src/xHttpRequest.h ../src/xHttpResponse.h ../src/xTcpConnection.h
