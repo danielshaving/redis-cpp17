6dc772137abbc3541d4a2a3aa9e54993bf71d910
danielshaving <474711079@qq.com>
2018-06-11 11:33:17 +0800

    rediscli

--- src/client.d
+++
@@ -1,5 +0,0 @@
-client.o: ../src/client.cpp ../src/all.h ../src/tcpclient.h \
- ../src/eventloop.h ../src/channel.h ../src/socket.h ../src/epoll.h \
- ../src/stringpiece.h ../src/log.h ../src/buffer.h ../src/timerqueue.h \
- ../src/callback.h ../src/timer.h ../src/zmalloc.h ../src/tcpconnection.h \
- ../src/threadpool.h
